7644f5d0f62f78d0379fa91a7c998b0959ed7121
Md Ariful Islam <arifulapece@gmail.com>
2018-12-09 16:02:01 +0300

    Create certer-button.php

---
+++ certer-button.php
@@ -0,0 +1,94 @@
+<head>
+<style>
+.button1{
+width: 150px;
+height: 50px;
+background-color: #3498db;
+text-decoration: none;
+border: none;
+color: white;
+font-size: 20px;
+
+  background-image: -webkit-linear-gradient(top, #3498db, #2980b9);
+  background-image: -moz-linear-gradient(top, #3498db, #2980b9);
+  background-image: -ms-linear-gradient(top, #3498db, #2980b9);
+  background-image: -o-linear-gradient(top, #3498db, #2980b9);
+  background-image: linear-gradient(to bottom, #3498db, #2980b9);
+  -webkit-border-radius: 12;
+  -moz-border-radius: 12;
+  border-radius: 12px;
+  padding: 10px 20px 10px 20px;
+
+}
+
+.button1:hover {
+  background: #3cb0fd;
+  background-image: -webkit-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -moz-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -ms-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -o-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: linear-gradient(to bottom, #3cb0fd, #3498db);
+  text-decoration: none;
+}
+
+.button2{
+width: 150px;
+height: 50px;
+background-color: #3498db;
+text-decoration: none;
+border: none;
+color: white;
+font-size: 20px;
+
+  background-image: -webkit-linear-gradient(top, #3498db, #2980b9);
+  background-image: -moz-linear-gradient(top, #3498db, #2980b9);
+  background-image: -ms-linear-gradient(top, #3498db, #2980b9);
+  background-image: -o-linear-gradient(top, #3498db, #2980b9);
+  background-image: linear-gradient(to bottom, #3498db, #2980b9);
+  -webkit-border-radius: 12;
+  -moz-border-radius: 12;
+  border-radius: 12px;
+  padding: 10px 20px 10px 20px;
+
+}
+
+.button2:hover {
+  background: #3cb0fd;
+  background-image: -webkit-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -moz-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -ms-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -o-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: linear-gradient(to bottom, #3cb0fd, #3498db);
+  text-decoration: none;
+}
+
+.wrapper {
+  text-align: center;
+  display: inline-block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -25px;
+  margin-left: -151px;
+}
+
+
+body {
+    background-color: #fff4fc;
+}
+</style>
+
+</head>
+
+<body>
+<?php /* Template Name: center_button */ ?>
+
+<div class="wrapper" >
+
+ <a href="#"><button type="button home-button" class="button1" >Button1</button></a>
+ 
+ <a href="#"><button type="button contact-button" class="button2">Button2</button></a>
+ 
+</div>
+</body>
+ </html>
